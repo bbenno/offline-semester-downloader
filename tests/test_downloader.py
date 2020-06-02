9e3a40586d82463209e1d02cbf8e9429acc1883f
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-06-02 08:13:03 +0200

    Downloader tests now using .env variables

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 import os
-from src.downloader import ZoomDownloader
+from dotenv import load_dotenv
+from osd.downloader import _ZoomDownloader
 
 
 class TestZoomDownloader:
     def test_download(self, tmp_path):
+        load_dotenv()
+
         file_path = tmp_path / "a.mp4"
-        zd = ZoomDownloader()
-        url = os.getenv('ZOOM_CLOUD_TEST_URL')
+        zd = _ZoomDownloader()
+        url = os.getenv("ZOOM_CLOUD_TEST_URL")
         assert url
         zd.download(url, file_path)
         assert file_path.is_file()
